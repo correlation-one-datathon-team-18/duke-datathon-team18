6072e8d3b87f4c8daf627d9032f4df4951e73be0
intellectape <adi_b09@hotmail.com>
2017-04-21 14:08:46 -0400

    Adding comments to the Time Series code.

--- Code/timeSeries_dataclean.R
+++ Code/timeSeries_dataclean.R
@@ -1,10 +1,10 @@
 library(forecast)
-setwd('/Users/ADITYA/Desktop/Datathon/git-repo/duke-datathon-team18/')
+setwd('/Users/ADITYA/Desktop/Datathon/git-repo/duke-datathon-team18/Data/')
 data <- read.csv('econ_state.csv')
 data_2 <- read.csv('econ_federal.csv')
 real_estate <- read.csv('real_estate.csv')
 
-
+# Time Series function
 timeSeries <- function(datatype){
   if(datatype == 'Unemployement'){
     myts <- ts(data_2$unemployment_rate, start=c(1954, 8), end=c(2017, 01), frequency=12) 
@@ -36,8 +36,11 @@ timeSeries <- function(datatype){
   
 }
 
-cityData <- read.csv('NY_predict.csv', header = TRUE)
-datacity <- function(city){
+# function for creating CSV file for a City in the State
+# Data: City, Real Estate, GDP, Per Capita, Unmployment Rate, Employment Rate
+# Years in rows: 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016
+datacity <- function(city, state){
+  cityData <- read.csv(paste(state, "csv", sep = '.'), header = TRUE)
   df1 <- data.frame(t(data.frame(cityData$real_2005[which(cityData$city==city)], cityData$real_2006[which(cityData$city==city)], cityData$real_2007[which(cityData$city==city)], cityData$real_2008[which(cityData$city==city)], cityData$real_2010[which(cityData$city==city)], cityData$real_2011[which(cityData$city==city)], cityData$real_2012[which(cityData$city==city)], cityData$real_2013[which(cityData$city==city)], cityData$real_2014[which(cityData$city==city)], cityData$real_2015[which(cityData$city==city)], cityData$real_2016[which(cityData$city==city)])), 
                     t(data.frame(cityData$gdp_2005[which(cityData$city==city)], cityData$gdp_2006[which(cityData$city==city)], cityData$gdp_2007[which(cityData$city==city)], cityData$gdp_2008[which(cityData$city==city)], cityData$gdp_2010[which(cityData$city==city)], cityData$gdp_2011[which(cityData$city==city)], cityData$gdp_2012[which(cityData$city==city)], cityData$gdp_2013[which(cityData$city==city)], cityData$gdp_2014[which(cityData$city==city)], cityData$gdp_2015[which(cityData$city==city)], cityData$gdp_2016[which(cityData$city==city)])), 
                     t(data.frame(cityData$per_capita_2005[which(cityData$city==city)], cityData$per_capita_2006[which(cityData$city==city)], cityData$per_capita_2007[which(cityData$city==city)], cityData$per_capita_2008[which(cityData$city==city)], cityData$per_capita_2010[which(cityData$city==city)], cityData$per_capita_2011[which(cityData$city==city)], cityData$per_capita_2012[which(cityData$city==city)], cityData$per_capita_2013[which(cityData$city==city)], cityData$per_capita_2014[which(cityData$city==city)], cityData$per_capita_2015[which(cityData$city==city)], cityData$per_capita_2016[which(cityData$city==city)])),
@@ -47,8 +50,14 @@ datacity <- function(city){
   write.csv(df1, file = paste(city, 'csv', sep = '.'), row.names = TRUE)
 }
 
+# function for creating CSV file for a State
+# Data: City, Real Estate, GDP, Per Capita, Unmployment Rate, Employment Rate
+# Years: 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016
 datastate <- function(state){
-  df <- data.frame(real_estate$city[which(real_estate$state == state)], real_estate$X2005[which(real_estate$state == state)], real_estate$X2006[which(real_estate$state == state)], real_estate$X2007[which(real_estate$state == state)], real_estate$X2008[which(real_estate$state == state)], real_estate$X2009[which(real_estate$state == state)], real_estate$X2010[which(real_estate$state == state)], real_estate$X2011[which(real_estate$state == state)], real_estate$X2012[which(real_estate$state == state)], real_estate$X2013[which(real_estate$state == state)], real_estate$X2014[which(real_estate$state == state)], real_estate$X2015[which(real_estate$state == state)], real_estate$X2016[which(real_estate$state == state)], data$X2005_gdp[which(data$state==state)], data$X2006_gdp[which(data$state==state)], data$X2007_gdp[which(data$state==state)], data$X2008_gdp[which(data$state==state)], data$X2009_gdp[which(data$state==state)], data$X2010_gdp[which(data$state==state)], data$X2011_gdp[which(data$state==state)], data$X2012_gdp[which(data$state==state)], data$X2013_gdp[which(data$state==state)], data$X2014_gdp[which(data$state==state)], data$X2015_gdp[which(data$state==state)], data$X2016_gdp[which(data$state==state)], data$X2005_per_capita[which(data$state==state)], data$X2006_per_capita[which(data$state==state)], data$X2007_per_capita[which(data$state==state)], data$X2008_per_capita[which(data$state==state)], data$X2009_per_capita[which(data$state==state)], data$X2010_per_capita[which(data$state==state)], data$X2011_per_capita[which(data$state==state)], data$X2012_per_capita[which(data$state==state)], data$X2013_per_capita[which(data$state==state)], data$X2014_per_capita[which(data$state==state)], data$X2015_per_capita[which(data$state==state)], data$X2016_per_capita[which(data$state==state)], data$X2005_ur[which(data$state==state)], data$X2006_ur[which(data$state==state)], data$X2007_ur[which(data$state==state)], data$X2008_ur[which(data$state==state)], data$X2009_ur[which(data$state==state)], data$X2010_ur[which(data$state==state)], data$X2011_ur[which(data$state==state)], data$X2012_ur[which(data$state==state)], data$X2013_ur[which(data$state==state)], data$X2014_ur[which(data$state==state)], data$X2015_ur[which(data$state==state)], data$X2016_ur[which(data$state==state)], 100 - data$X2005_ur[which(data$state==state)], 100 - data$X2006_ur[which(data$state==state)], 100 - data$X2007_ur[which(data$state==state)], 100 - data$X2008_ur[which(data$state==state)], 100 - data$X2009_ur[which(data$state==state)], 100 - data$X2010_ur[which(data$state==state)], 100 - data$X2011_ur[which(data$state==state)], 100 - data$X2012_ur[which(data$state==state)], 100 - data$X2013_ur[which(data$state==state)], 100 - data$X2014_ur[which(data$state==state)], 100 - data$X2015_ur[which(data$state==state)], 100 - data$X2016_ur[which(data$state==state)])
+  df <- data.frame(real_estate$city[which(real_estate$state == state)], real_estate$X2005[which(real_estate$state == state)], real_estate$X2006[which(real_estate$state == state)], real_estate$X2007[which(real_estate$state == state)], real_estate$X2008[which(real_estate$state == state)], real_estate$X2009[which(real_estate$state == state)], real_estate$X2010[which(real_estate$state == state)], real_estate$X2011[which(real_estate$state == state)], real_estate$X2012[which(real_estate$state == state)], real_estate$X2013[which(real_estate$state == state)], real_estate$X2014[which(real_estate$state == state)], real_estate$X2015[which(real_estate$state == state)], real_estate$X2016[which(real_estate$state == state)], 
+                   data$X2005_gdp[which(data$state==state)], data$X2006_gdp[which(data$state==state)], data$X2007_gdp[which(data$state==state)], data$X2008_gdp[which(data$state==state)], data$X2009_gdp[which(data$state==state)], data$X2010_gdp[which(data$state==state)], data$X2011_gdp[which(data$state==state)], data$X2012_gdp[which(data$state==state)], data$X2013_gdp[which(data$state==state)], data$X2014_gdp[which(data$state==state)], data$X2015_gdp[which(data$state==state)], data$X2016_gdp[which(data$state==state)], data$X2005_per_capita[which(data$state==state)], data$X2006_per_capita[which(data$state==state)], data$X2007_per_capita[which(data$state==state)], data$X2008_per_capita[which(data$state==state)], 
+                   data$X2009_per_capita[which(data$state==state)], data$X2010_per_capita[which(data$state==state)], data$X2011_per_capita[which(data$state==state)], data$X2012_per_capita[which(data$state==state)], data$X2013_per_capita[which(data$state==state)], data$X2014_per_capita[which(data$state==state)], data$X2015_per_capita[which(data$state==state)], data$X2016_per_capita[which(data$state==state)], data$X2005_ur[which(data$state==state)], data$X2006_ur[which(data$state==state)], data$X2007_ur[which(data$state==state)], data$X2008_ur[which(data$state==state)], data$X2009_ur[which(data$state==state)], data$X2010_ur[which(data$state==state)], data$X2011_ur[which(data$state==state)], data$X2012_ur[which(data$state==state)], data$X2013_ur[which(data$state==state)], data$X2014_ur[which(data$state==state)], data$X2015_ur[which(data$state==state)], data$X2016_ur[which(data$state==state)], 
+                   100 - data$X2005_ur[which(data$state==state)], 100 - data$X2006_ur[which(data$state==state)], 100 - data$X2007_ur[which(data$state==state)], 100 - data$X2008_ur[which(data$state==state)], 100 - data$X2009_ur[which(data$state==state)], 100 - data$X2010_ur[which(data$state==state)], 100 - data$X2011_ur[which(data$state==state)], 100 - data$X2012_ur[which(data$state==state)], 100 - data$X2013_ur[which(data$state==state)], 100 - data$X2014_ur[which(data$state==state)], 100 - data$X2015_ur[which(data$state==state)], 100 - data$X2016_ur[which(data$state==state)])
   
   names(df) <- c('city', 
                  'real_2005','real_2006', 'real_2007', 'real_2008', 'real_2009', 'real_2010', 'real_2011', 'real_2012', 'real_2013', 'real_2014', 'real_2015', 'real_2016', 
@@ -57,13 +66,17 @@ datastate <- function(state){
                  'ur_2005', 'ur_2006', 'ur_2007', 'ur_2008', 'ur_2009', 'ur_2010', 'ur_2011', 'ur_2012', 'ur_2013', 'ur_2014', 'ur_2015', 'ur_2016',
                  'er_2005', 'er_2006', 'er_2007', 'er_2008', 'er_2009', 'er_2010', 'er_2011', 'er_2012', 'er_2013', 'er_2014', 'er_2015', 'er_2016')
   
-  write.csv(df, file = "NY_predict.csv", row.names = TRUE)  
+  write.csv(df, file = paste(state, "csv", sep = '.'), row.names = TRUE)  
   
 }
 
+# Creating CSV files for the States
+for (i in unique(real_estate$state)){
+    datastate(i)
+}
 
+# Creating City files for the Particular State
 for (i in df$real_estate.city.which.real_estate.state.....NY...){
   datacity(i)
 }
 
-timeSeries('Unemployement')
